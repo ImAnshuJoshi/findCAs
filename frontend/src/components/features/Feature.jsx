dcea04157bc44d284da6253d0353f4a5a74b2354
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2023-10-17 23:48:01 +0530

    assests added

--- frontend/src/components/features/Feature.jsx
+++ frontend/src/components/features/Feature.jsx
@@ -1,7 +1,9 @@
 const Feature = () => {
   return (
     <div id="features" className="px-7 py-20">
-      <div className=" h-96  bg-slate-400">Roshan Jaiswal</div>
+      <div className=" h-96  bg-slate-400">Roshan Jaiswal
+      
+      </div>
     </div>
   );
 };
