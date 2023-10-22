fe2faec66a4631a31de56e0b509c26da2f311cff
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2023-10-22 15:21:51 +0530

    cadetails update

---
+++ frontend/src/components/recommCards/RecommendationCards.jsx
@@ -0,0 +1,10 @@
+
+const RecommendationCards = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RecommendationCards
--- frontend/src/pages/caDetails/CaDetails.jsx
+++ frontend/src/pages/caDetails/CaDetails.jsx
@@ -2,6 +2,7 @@ import NavBar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 import StarIcon from "@mui/icons-material/Star";
 import CalendarMonth from "@mui/icons-material/CalendarMonth";
+import RecommendationCards from "../../components/recommCards/RecommendationCards";
 const caDetails = () => {
   return (
     <div>
@@ -34,13 +35,26 @@ const caDetails = () => {
                 </span>
               </div>
               <div className=" my-6 flex justify-between gap-2">
-                  <button className=" w-[220px] font-popp bg-primary text-white py-[10px] px-5 rounded-[10px] hover:bg-blue-400">Request Proposal</button>
-                  <button className=" w-[220px] font-popp border-[1.5px] border-primary bg-white text-primary py-[10px] px-5 rounded-[10px] hover:bg-slate-400">Chat with me</button>
+                <button className=" w-[220px] font-popp bg-primary text-white py-[10px] px-5 rounded-[10px] hover:bg-blue-400">
+                  Request Proposal
+                </button>
+                <button className=" w-[220px] font-popp border-[1.5px] border-primary bg-white text-primary py-[10px] px-5 rounded-[10px] hover:bg-slate-400">
+                  Chat with me
+                </button>
               </div>
             </div>
           </div>
-          <div>
-            Roshan Jaiswal
+          <div className="rounded-[20px] shadow-[2px_4px_15px_0_rgba(133,133,167,0.25)] mt-16 md:mt-8 w-full  px-7">
+            <p className=" font-popp text-4xl font-bold py-7">
+              What people say?
+            </p>
+            <p className=" text-xl font-popp pb-7">
+              I cannot express enough gratitude for the support Micheal has
+              provided in managing my personal finances. Their attention to
+              detail and deep understanding of financial markets has ensured
+              that my investments are in safe hands. I highly recommend their
+              services to anyone seeking financial guidance.
+            </p>
           </div>
         </div>
         {/* .............................................. */}
@@ -49,9 +63,59 @@ const caDetails = () => {
             src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
             alt=""
             className=" w-[733px] h-[412px]"
-          />{" "}
+          />
+          <div>
+            <p className="font-popp text-4xl font-bold my-8">
+              About Michael Jackson
+            </p>
+            <div className="grid grid-cols-3 gap-8">
+              <div className="">
+                <p className="font-popp font-bold text-[#999] leading-[34px]">
+                  FROM
+                </p>
+                <p className="font-popp text-xl leading-[34px]">INDIA</p>
+              </div>
+              <div>
+                <p className="font-popp font-bold text-[#999] leading-[34px]">
+                  PARTNER SINCE
+                </p>
+                <p className="font-popp text-xl leading-[34px]">2011</p>
+              </div>
+              <div>
+                <p className="font-popp font-bold text-[#999] leading-[34px]">
+                  AVERAGE RESPONSE TIME
+                </p>
+                <p className="font-popp text-xl leading-[34px]">30 minutes</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 grid md:grid-cols-2 gap-8 ">
+            <div>
+              <p className="font-popp font-bold leading-8  text-[#999]">
+                SERVICES I OFFER
+              </p>
+              <ul className=" list-disc list-inside font-popp text-xl space-y-1">
+                <li>Financial accounting</li>
+                <li>Financial statements </li>
+                <li>Bookkeeping</li>
+                <li>Maintain Charts of Accounts</li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-popp font-bold leading-8  text-[#999]">
+              WHY ME?
+              </p>
+              <ul className=" list-disc list-inside font-popp text-xl space-y-1">
+                <li>One-time delivery</li>
+                <li>24/7 customer support </li>
+                <li>Error-free documents</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
+      <RecommendationCards/>
       <Footer />
     </div>
   );
