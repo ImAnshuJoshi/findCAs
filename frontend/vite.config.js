ca015ede201e725efde65193bf081cc593f9728f
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2023-11-03 18:11:50 +0530

    config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactScroll from 'vite-plugin-react-scroll';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactScroll()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-scroll'],
+    },
+  },
+  
 });
