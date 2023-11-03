937b32622c4613ffaf4ab14075811207b1ecfae5
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2023-11-03 18:04:36 +0530

    config file change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-scroll'],
-    },
-    chunkSizeWarningLimit: 2000,
+  optimizeDeps: {
+    include: ['react-scroll'],
   },
 });
