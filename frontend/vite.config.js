23dd655fce19ca543c36e6c23e1315a4cbbc7f6c
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-04-10 17:02:23 +0530

    update vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,27 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Handle image imports
+        {
+          name: 'image-import',
+          resolveId(source) {
+            if (source.endsWith('.jpeg') || source.endsWith('.jpg') || source.endsWith('.png') || source.endsWith('.gif')) {
+              return source; // tell Rollup this is an image
+            }
+            return null; // otherwise, it remains unresolved
+          },
+          load(id) {
+            if (id.endsWith('.jpeg') || id.endsWith('.jpg') || id.endsWith('.png') || id.endsWith('.gif')) {
+              return `export default import.meta.ROLLUP_FILE_URL_${id}`; // use Rollup's file loader
+            }
+            return null;
+          },
+        },
+      ],
+    },
+  },
 });
